3e2a2ea7b55d24ab38b9c5b4bde488c8b69d2310
Marcos Silva <vinicius_marcosmartins@hotmail.com>
2018-12-11 19:04:24 -0200

    Criando função de carregar escudo dos times

--- js/controllers/indexCtrl.js
+++ js/controllers/indexCtrl.js
@@ -1,16 +1,17 @@
 app.controller("indexCtrl", function($scope, $http){
     $scope.loading = 0;
     $scope.dados = {};
+    var escudo;
 
     var carregarPartidas = function(c){
         $scope.loading++;
-        $http.get("https://api.football-data.org/v2/competitions/"+ c +"/matches", {
+        $http.get("https://api.football-data.org/v2/competitions/"+ c +"/matches?status=SCHEDULED", {
             headers: {'X-Auth-Token': '4fb9f4ae0f5b414788d01798280a741d'}
         }).then(function(response){
             window.sessionStorage.setItem(c, JSON.stringify(response.data));
             $scope.dados[c] = response.data;
         }, function(erro){
-            console.log(erro);
+            console.log(erro.data);
         }).finally(function(){
             $scope.loading--;
         });
@@ -25,4 +26,23 @@ app.controller("indexCtrl", function($scope, $http){
             console.log($scope.dados);
         }
     }
+    var carregarEscudo = function(id){
+        $http.get("http://api.football-data.org/v2/teams/" + id, {
+            headers: {'X-Auth-Token': '4fb9f4ae0f5b414788d01798280a741d'}
+        }).then(function(response){
+            escudo = response.data.crestUrl;
+            console.log(escudo);
+        });
+    }
+    $scope.getEscudo = function(id){
+        carregarEscudo(id);
+        return escudo;
+    }
+    /*
+    $scope.getDisplay = function($event){
+        if($event.currentTarget.parentNode.style.display == "block"){
+            return true;
+        }
+    }
+    */
 })
